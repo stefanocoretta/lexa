1602f480953a3655d0d1a0c9dee05cd9c42e3899
stefanocoretta <stefano.coretta@gmail.com>
2022-01-12 22:16:39 +0000

    ♻️ - add search function

--- R/lexicon.R
+++ R/lexicon.R
@@ -1,15 +1,25 @@
+# Add entries ----
+
+#' Add entry to lexicon
+#'
+#' This function creates a new entry in the lexicon, i.e. a new empty entry
+#' skeleton is written to disk, in the `lexicon/` directory, for the user
+#' to edit at will.
+#'
+#' @param lexadb A `lexadb` object (created with \code{\link{load_lexadb}})
+#'
+#' @return Nothing. Used for its side effects
+#' @export
 add_entry <- function(lexadb) {
   lx_entry <- create_entry(lexadb)
   write_entry(lexadb, lx_entry)
 }
 
-write_entry <- function(lexadb, lx_entry) {
-  db_path <- attr(lexadb, "meta")$path
-  lx_path <- file.path("lexicon", paste0(lx_entry$id, ".yaml"))
-  lx_full_path <- file.path(db_path, lx_path)
-  yaml::write_yaml(lx_entry, lx_full_path)
-}
+# Write entry helpers ----
+#
+# The following are helper functions used when creating a new lexical entry.
 
+# Check last entry ID and increase hex by 1.
 create_lx_id <- function(lexadb) {
   db_path <- attr(lexadb, "meta")$path
   lx_files <- list.files(file.path(db_path, "lexicon"), pattern = "*.yaml")
@@ -22,6 +32,7 @@ create_lx_id <- function(lexadb) {
   return(new_id)
 }
 
+# Prepare empty entry skeleton.
 create_entry <- function(lexadb = NULL) {
   list(
     id = ifelse(is.null(lexadb), "lx_000001", create_lx_id(lexadb)),
@@ -51,5 +62,27 @@ create_entry <- function(lexadb = NULL) {
     ),
     date_created = as.character(Sys.time()),
     date_modified = as.character(Sys.time())
+  )
+}
+
+# Actually writes entry on disk in lexicon/.
+write_entry <- function(lexadb, lx_entry) {
+  db_path <- attr(lexadb, "meta")$path
+  lx_path <- file.path("lexicon", paste0(lx_entry$id, ".yaml"))
+  lx_full_path <- file.path(db_path, lx_path)
+  yaml::write_yaml(lx_entry, lx_full_path)
+}
+
+
+
+# Search entries ----
+
+search_lexicon <- function(lexadb, pattern) {
+  db_path <- attr(lexadb, "meta")$path
+  lexicon <- read_lexicon(db_path)
+
+  lapply(lexicon, function(x) {
+      stringr::str_detect(x$lexeme, pattern)
+    }
   )
 }
