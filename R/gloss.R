a45353a93ecb59e33f54f282e15b951b2c897797
stefanocoretta <stefano.coretta@gmail.com>
2022-02-05 18:08:26 +0000

    ✨ - add phon line in glosses if available

--- R/gloss.R
+++ R/gloss.R
@@ -14,30 +14,51 @@ typeset_gloss <- function(lexadb, text, sentence, format = "latex") {
   text <- yaml::read_yaml(file.path(db_path, "texts", paste0(text, ".yaml")))
 
   sentence_i <- text$sentences[[sentence]]$sentence
+  phon <- text$sentences[[sentence]]$phon
   morpho <- text$sentences[[sentence]]$morpho
   gloss <- text$sentences[[sentence]]$gloss
   translation <- text$sentences[[sentence]]$translation
 
   if (format == "latex") {
-    glue::glue(
-      "```{{=latex}}
-      \\ex \\begingl
-      \\glpreamble {sentence_i}//
-      \\gla {morpho}//
-      \\glb {gloss}//
-      \\glft {translation}//
-      \\endgl \\xe
-      ```"
-    )
+    if (!is.na(phon)) {
+      gloss_ex <- glue::glue(
+        "```{{=latex}}
+        \\ex \\begingl
+        \\glpreamble {sentence_i}//
+        \\glpreamble {phon}//
+        \\gla {morpho}//
+        \\glb {gloss}//
+        \\glft {translation}//
+        \\endgl \\xe
+        ```"
+      )
+    } else {
+      gloss_ex <- glue::glue(
+        "```{{=latex}}
+        \\ex \\begingl
+        \\glpreamble {sentence_i}//
+        \\gla {morpho}//
+        \\glb {gloss}//
+        \\glft {translation}//
+        \\endgl \\xe
+        ```"
+      )
+
+      return(gloss_ex)
+    }
+
 
   } else if (format == "html") {
-    htmltools::div(
+    gloss_div <- htmltools::div(
       "data-gloss" = NA,
-      htmltools::p(glue::glue("{sentence_i}")),
+      htmltools::p(class = "gloss__line--original", glue::glue("{sentence_i}")),
+      htmltools::p(class = "gloss__line--original", style = "font-weight: 400;", glue::glue("[{phon}]")),
       htmltools::p(glue::glue("{morpho}")),
       htmltools::p(glue::glue("{gloss}")),
       htmltools::p(glue::glue("\u2018{translation}\u2019")),
     )
+
+    return(gloss_div)
   }
 
 }
